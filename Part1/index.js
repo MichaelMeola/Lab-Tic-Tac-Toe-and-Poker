9dd32a0a0281592c5cdc5c0efb75bf01b771bca4
Michael Meola <meolaadonai@gmail.com>
2023-11-09 16:39:22 -0700

    Finished

---
+++ Part1/index.js
@@ -0,0 +1,66 @@
+let player = `X`
+
+function play (evt) {
+    const targetSquare = evt.target
+    targetSquare.innerText = player
+
+    if (player === 'X') {
+        player = 'O'
+      } else {
+        player = 'X'
+      }
+    
+      const playerSpan = document.querySelector('#current-player')
+      playerSpan.innerText = player
+
+      const winner = calculateWinner()
+        if (winner) {
+    alert(`${winner} is the winner!`)
+        } else if (isBoardFull()) {
+    alert('Game is a tie!')
+  }
+}
+function isBoardFull() {
+    for (const square of squares) {
+      if (square.innerText === '') {
+        return false
+      }
+    }
+    return true
+  }
+
+const squares = document.querySelectorAll(`.square`)
+
+for (const square of squares) {
+    square.addEventListener('click', play)
+}
+
+function calculateWinner() {
+    const lines = [
+      // Horizontal lines
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Vertical lines
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonal lines
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (const line of lines) {
+      const [a, b, c] = line
+      
+      const squareAText = squares[a].innerText
+      const squareBText = squares[b].innerText
+      const squareCText = squares[c].innerText
+  
+      if (squareAText !== '' && squareAText === squareBText && squareAText === squareCText) {
+        return squareAText
+      }
+    }
+  
+    return undefined
+  }
---
+++ Part2/index.js
@@ -0,0 +1,12 @@
+const idInput = document.querySelector('#idInput')
+const colorInput = document.querySelector('#colorInput')
+
+function setCard() {
+    const card = document.querySelector(`#${idInput.value}`)
+    console.log(card)
+    card.style.color = colorInput.value
+  }
+  
+  document.querySelector("#editCardButton").addEventListener('click', setCard)
+
+  
